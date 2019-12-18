efd7835067a574310c7fa47cc2ae6dc4aa41e766
Johann Rolschewski <johann@rolschewski.de>
2019-12-18 09:20:36 +0100

    1.01

--- lib/PICA/Data.pm
+++ lib/PICA/Data.pm
@@ -2,7 +2,7 @@ package PICA::Data;
 use strict;
 use warnings;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 use Exporter 'import';
 our @EXPORT_OK = qw(pica_parser pica_writer pica_path pica_xml_struct
--- lib/PICA/Path.pm
+++ lib/PICA/Path.pm
@@ -2,7 +2,7 @@ package PICA::Path;
 use strict;
 use warnings;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 use Carp qw(confess);
 use Scalar::Util qw(reftype);
