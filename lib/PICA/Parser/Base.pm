7b92b188cc12cd537ac9ac16e4e0d086ec6e7e08
Jakob Voss <voss@gbv.de>
2021-04-30 12:28:33 +0200

    Support parsing annotated PICA

--- lib/PICA/Parser/Base.pm
+++ lib/PICA/Parser/Base.pm
@@ -12,9 +12,10 @@ sub _new {
     my (%options) = @_ % 2 ? (fh => @_) : @_;
 
     bless {
-        bless  => !!$options{bless},
-        strict => !!$options{strict},
-        fh     => defined $options{fh} ? $options{fh} : \*STDIN
+        bless     => !!$options{bless},
+        strict    => !!$options{strict},
+        fh        => defined $options{fh} ? $options{fh} : \*STDIN,
+        annotated => $options{annotated}
     }, $class;
 }
 
@@ -120,6 +121,11 @@ Return records as instances of L<PICA::Data> (disabled by default).
 
 By default faulty fields in records are skipped with warnings. You can make
 them fatal by setting the I<strict> parameter to 1.
+
+=item annotated
+
+By default some parsers also support annotated PICA. Set to true to enforce
+field annotations or to false to forbid them.
 
 =back
 
