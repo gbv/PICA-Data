17151c3c02c0a6f84599909002005a24d3575d14
Jakob Voss <voss@gbv.de>
2021-06-07 23:05:49 +0200

    Don't warn when writing empty records

--- lib/PICA/Writer/Base.pm
+++ lib/PICA/Writer/Base.pm
@@ -62,7 +62,12 @@ sub write_identifier {
 
 sub write_record {
     my ($self, $record) = @_;
-    $record = clean_pica($record, allow_empty_subfields => 1,) or return;
+    $record = clean_pica(
+        $record,
+        allow_empty_subfields => 1,
+        ignore_empty_records  => 1
+    ) or return;
+    return unless @$record;
 
     my $fh = $self->{fh};
     $self->write_field($_) for @$record;
